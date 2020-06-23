f30e22e3ee0b3599a82e578175325fb2792b6a4f
sizz <ssasun94@gmail.com>
2020-06-23 17:37:30 +0900

    update: lambda

--- ch6/lambda/index.js
+++ ch6/lambda/index.js
@@ -5,7 +5,7 @@ const S3 = new AWS.S3({ region: 'us-east-2' });
 
 exports.handler = async (event, context, callback) => {
     const Bucket = event.Records[0].s3.bucket.name;
-    const Key = event.Records[0].s3.object.key;
+    const Key = decodeURIComponent(event.Records[0].s3.object.key);
     const filename = Key.split('/')[Key.split('/').length - 1];
     const ext = Key.split('.')[Key.split('.').length - 1];
     console.log(Key, filename, ext);
